b32e0a90aac498d2a35f106edd4ee489e16102c5
Ben Wallace <beenwii@yahoo.com>
2020-02-25 08:08:47 -0700

    Add project files.

---
+++ DinerMenuForm.Designer.vb
@@ -0,0 +1,282 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class DinerMenuForm
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.SaladButton = New System.Windows.Forms.Button()
+        Me.FishButton = New System.Windows.Forms.Button()
+        Me.ExitButton = New System.Windows.Forms.Button()
+        Me.DisplaySpecialLabel = New System.Windows.Forms.Label()
+        Me.DinerNameLabel = New System.Windows.Forms.Label()
+        Me.SoupButton = New System.Windows.Forms.Button()
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        Me.DressingOptions = New System.Windows.Forms.Panel()
+        Me.RadioButton3 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton2 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton1 = New System.Windows.Forms.RadioButton()
+        Me.SideOptions = New System.Windows.Forms.Panel()
+        Me.RadioButton7 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton4 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton5 = New System.Windows.Forms.RadioButton()
+        Me.RadioButton6 = New System.Windows.Forms.RadioButton()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.DressingOptions.SuspendLayout()
+        Me.SideOptions.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'SaladButton
+        '
+        Me.SaladButton.Location = New System.Drawing.Point(224, 372)
+        Me.SaladButton.Name = "SaladButton"
+        Me.SaladButton.Size = New System.Drawing.Size(155, 121)
+        Me.SaladButton.TabIndex = 1
+        Me.SaladButton.Text = "Salad"
+        Me.SaladButton.UseVisualStyleBackColor = True
+        '
+        'FishButton
+        '
+        Me.FishButton.Location = New System.Drawing.Point(403, 372)
+        Me.FishButton.Name = "FishButton"
+        Me.FishButton.Size = New System.Drawing.Size(163, 121)
+        Me.FishButton.TabIndex = 2
+        Me.FishButton.Text = "Fish"
+        Me.FishButton.UseVisualStyleBackColor = True
+        '
+        'ExitButton
+        '
+        Me.ExitButton.DialogResult = System.Windows.Forms.DialogResult.Cancel
+        Me.ExitButton.Location = New System.Drawing.Point(763, 372)
+        Me.ExitButton.Name = "ExitButton"
+        Me.ExitButton.Size = New System.Drawing.Size(146, 121)
+        Me.ExitButton.TabIndex = 3
+        Me.ExitButton.Text = "E&xit"
+        Me.ExitButton.UseVisualStyleBackColor = True
+        '
+        'DisplaySpecialLabel
+        '
+        Me.DisplaySpecialLabel.BackColor = System.Drawing.SystemColors.ButtonFace
+        Me.DisplaySpecialLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DisplaySpecialLabel.Location = New System.Drawing.Point(537, 43)
+        Me.DisplaySpecialLabel.Name = "DisplaySpecialLabel"
+        Me.DisplaySpecialLabel.Size = New System.Drawing.Size(475, 326)
+        Me.DisplaySpecialLabel.TabIndex = 4
+        Me.DisplaySpecialLabel.Text = "Please choose from one of the options below"
+        Me.DisplaySpecialLabel.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'DinerNameLabel
+        '
+        Me.DinerNameLabel.AutoSize = True
+        Me.DinerNameLabel.Font = New System.Drawing.Font("Microsoft Sans Serif", 20.0!, CType((System.Drawing.FontStyle.Bold Or System.Drawing.FontStyle.Underline), System.Drawing.FontStyle), System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.DinerNameLabel.Location = New System.Drawing.Point(461, 9)
+        Me.DinerNameLabel.Name = "DinerNameLabel"
+        Me.DinerNameLabel.Size = New System.Drawing.Size(164, 31)
+        Me.DinerNameLabel.TabIndex = 12
+        Me.DinerNameLabel.Text = "Ben's Diner"
+        '
+        'SoupButton
+        '
+        Me.SoupButton.Location = New System.Drawing.Point(30, 372)
+        Me.SoupButton.Name = "SoupButton"
+        Me.SoupButton.Size = New System.Drawing.Size(165, 121)
+        Me.SoupButton.TabIndex = 0
+        Me.SoupButton.Text = "Soup"
+        Me.SoupButton.UseVisualStyleBackColor = True
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = Global.Diner_Menu.My.Resources.Resources.logo
+        Me.PictureBox1.Location = New System.Drawing.Point(11, 43)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(520, 309)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 13
+        Me.PictureBox1.TabStop = False
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.CheckBox1.Location = New System.Drawing.Point(663, 280)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(246, 30)
+        Me.CheckBox1.TabIndex = 0
+        Me.CheckBox1.Text = "Remove Red Peppers"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        Me.CheckBox1.Visible = False
+        '
+        'DressingOptions
+        '
+        Me.DressingOptions.Controls.Add(Me.RadioButton3)
+        Me.DressingOptions.Controls.Add(Me.RadioButton2)
+        Me.DressingOptions.Controls.Add(Me.RadioButton1)
+        Me.DressingOptions.Location = New System.Drawing.Point(643, 252)
+        Me.DressingOptions.Name = "DressingOptions"
+        Me.DressingOptions.Size = New System.Drawing.Size(240, 100)
+        Me.DressingOptions.TabIndex = 15
+        Me.DressingOptions.Visible = False
+        '
+        'RadioButton3
+        '
+        Me.RadioButton3.AutoSize = True
+        Me.RadioButton3.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton3.Location = New System.Drawing.Point(41, 60)
+        Me.RadioButton3.Name = "RadioButton3"
+        Me.RadioButton3.Size = New System.Drawing.Size(180, 30)
+        Me.RadioButton3.TabIndex = 2
+        Me.RadioButton3.TabStop = True
+        Me.RadioButton3.Text = "Oil and Vinegar"
+        Me.RadioButton3.UseVisualStyleBackColor = True
+        '
+        'RadioButton2
+        '
+        Me.RadioButton2.AutoSize = True
+        Me.RadioButton2.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton2.Location = New System.Drawing.Point(41, 37)
+        Me.RadioButton2.Name = "RadioButton2"
+        Me.RadioButton2.Size = New System.Drawing.Size(165, 30)
+        Me.RadioButton2.TabIndex = 1
+        Me.RadioButton2.TabStop = True
+        Me.RadioButton2.Text = "LowFat Italian"
+        Me.RadioButton2.UseVisualStyleBackColor = True
+        '
+        'RadioButton1
+        '
+        Me.RadioButton1.AutoSize = True
+        Me.RadioButton1.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton1.Location = New System.Drawing.Point(41, 14)
+        Me.RadioButton1.Name = "RadioButton1"
+        Me.RadioButton1.Size = New System.Drawing.Size(180, 30)
+        Me.RadioButton1.TabIndex = 0
+        Me.RadioButton1.TabStop = True
+        Me.RadioButton1.Text = "Italian Dressing"
+        Me.RadioButton1.UseVisualStyleBackColor = True
+        '
+        'SideOptions
+        '
+        Me.SideOptions.Controls.Add(Me.RadioButton7)
+        Me.SideOptions.Controls.Add(Me.RadioButton4)
+        Me.SideOptions.Controls.Add(Me.RadioButton5)
+        Me.SideOptions.Controls.Add(Me.RadioButton6)
+        Me.SideOptions.Location = New System.Drawing.Point(663, 250)
+        Me.SideOptions.Name = "SideOptions"
+        Me.SideOptions.Size = New System.Drawing.Size(258, 119)
+        Me.SideOptions.TabIndex = 1
+        Me.SideOptions.Visible = False
+        '
+        'RadioButton7
+        '
+        Me.RadioButton7.AutoSize = True
+        Me.RadioButton7.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton7.Location = New System.Drawing.Point(10, 86)
+        Me.RadioButton7.Name = "RadioButton7"
+        Me.RadioButton7.Size = New System.Drawing.Size(151, 30)
+        Me.RadioButton7.TabIndex = 6
+        Me.RadioButton7.TabStop = True
+        Me.RadioButton7.Text = "French Fries"
+        Me.RadioButton7.UseVisualStyleBackColor = True
+        '
+        'RadioButton4
+        '
+        Me.RadioButton4.AutoSize = True
+        Me.RadioButton4.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton4.Location = New System.Drawing.Point(10, 58)
+        Me.RadioButton4.Name = "RadioButton4"
+        Me.RadioButton4.Size = New System.Drawing.Size(251, 30)
+        Me.RadioButton4.TabIndex = 5
+        Me.RadioButton4.TabStop = True
+        Me.RadioButton4.Text = "Spaghettic W/Marinara"
+        Me.RadioButton4.UseVisualStyleBackColor = True
+        '
+        'RadioButton5
+        '
+        Me.RadioButton5.AutoSize = True
+        Me.RadioButton5.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton5.Location = New System.Drawing.Point(10, 35)
+        Me.RadioButton5.Name = "RadioButton5"
+        Me.RadioButton5.Size = New System.Drawing.Size(165, 30)
+        Me.RadioButton5.TabIndex = 4
+        Me.RadioButton5.TabStop = True
+        Me.RadioButton5.Text = "LowFat Italian"
+        Me.RadioButton5.UseVisualStyleBackColor = True
+        '
+        'RadioButton6
+        '
+        Me.RadioButton6.AutoSize = True
+        Me.RadioButton6.Font = New System.Drawing.Font("Microsoft Sans Serif", 16.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.RadioButton6.Location = New System.Drawing.Point(10, 12)
+        Me.RadioButton6.Name = "RadioButton6"
+        Me.RadioButton6.Size = New System.Drawing.Size(108, 30)
+        Me.RadioButton6.TabIndex = 3
+        Me.RadioButton6.TabStop = True
+        Me.RadioButton6.Text = "Broccoli"
+        Me.RadioButton6.UseVisualStyleBackColor = True
+        '
+        'DinerMenuForm
+        '
+        Me.AcceptButton = Me.SoupButton
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.AutoSize = True
+        Me.CancelButton = Me.ExitButton
+        Me.ClientSize = New System.Drawing.Size(1024, 505)
+        Me.Controls.Add(Me.SideOptions)
+        Me.Controls.Add(Me.DressingOptions)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.SoupButton)
+        Me.Controls.Add(Me.DinerNameLabel)
+        Me.Controls.Add(Me.DisplaySpecialLabel)
+        Me.Controls.Add(Me.ExitButton)
+        Me.Controls.Add(Me.FishButton)
+        Me.Controls.Add(Me.SaladButton)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.Name = "DinerMenuForm"
+        Me.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen
+        Me.Text = "Ben's Olive Garden Menu"
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.DressingOptions.ResumeLayout(False)
+        Me.DressingOptions.PerformLayout()
+        Me.SideOptions.ResumeLayout(False)
+        Me.SideOptions.PerformLayout()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents SaladButton As Button
+    Friend WithEvents FishButton As Button
+    Friend WithEvents ExitButton As Button
+    Friend WithEvents DisplaySpecialLabel As Label
+    Friend WithEvents DinerNameLabel As Label
+    Friend WithEvents SoupButton As Button
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents DressingOptions As Panel
+    Friend WithEvents RadioButton3 As RadioButton
+    Friend WithEvents RadioButton2 As RadioButton
+    Friend WithEvents RadioButton1 As RadioButton
+    Friend WithEvents SideOptions As Panel
+    Friend WithEvents RadioButton4 As RadioButton
+    Friend WithEvents RadioButton5 As RadioButton
+    Friend WithEvents RadioButton6 As RadioButton
+    Friend WithEvents RadioButton7 As RadioButton
+End Class
---
+++ DinerMenuForm.vb
@@ -0,0 +1,67 @@
+﻿'Ben Wallace
+'RCET0265
+'Spring 2020
+'Diner Menu Program
+
+Public Class DinerMenuForm
+    'Define values for each option of text for the text label
+    Private ReadOnly soup1 As String = "Your choice of one homemade soup:" & vbNewLine & vbNewLine & "Chicken & Gnocchi - A creamy soup made with roasted chicken, tradition italian dumplings and spinach." & vbNewLine & vbNewLine & "Pasta e Fagioli - White and red beans, ground beef, fresh tomatoes and tubetti pasta in a savory broth."
+    Private ReadOnly soup2 As String = "Your choice of one homemade soup:" & vbNewLine & vbNewLine & "Minestrone - Fresh vegetables, beans and pasta in a light tomato broth - a vegan classic." & vbNewLine & vbNewLine & "Zuppa Toscana - Spicy Italian sausage, fresh kale and russet potatoes in a creamy broth. Made with gluten-free ingredients."
+    Private ReadOnly salad2 As String = "Famous House Salad without croutons" & vbNewLine & vbNewLine & "Our famous house salad prepared without croutons for a gluten free alternative." & vbNewLine & vbNewLine & "Only 110 calories!"
+    Private ReadOnly salad1 As String = "Our Famous House Salad" & vbNewLine & vbNewLine & "Our Famous House Salad served with our signature Italian dressing(served on the side). 290 calories." & vbNewLine & vbNewLine & "Customize your dressing from one of the following options:" & vbNewLine & vbNewLine & vbNewLine & vbNewLine & vbNewLine 'Italian Dressing, LowFat Italian, or Oil & vinegar"
+    Private ReadOnly fish1 As String = "Salmon Picacata" & vbNewLine & vbNewLine & "Coho filet grilled to perfection, topped with a lemon garlic butter sauce, sun-dried tomatoes and capers. Served with parmesan-crusted zucchini," & vbNewLine & vbNewLine & "Choose your side:" & vbNewLine & vbNewLine & vbNewLine & vbNewLine & vbNewLine & vbNewLine  'Broccoli, Mashed Potatoes, Spaghetti w/Marinara, or French Fries."
+    Private ReadOnly fish2 As String = "Chicken and Shrimp Carbonara" & vbNewLine & vbNewLine & "Sauteed seasoned chicken, shrimp and spaghetti tossed in a creamy sauce with bacon and roasted red peppers."
+
+    Private Sub ExitButton_Click(sender As Object, e As EventArgs) Handles ExitButton.Click
+        'click here to close the program
+        Me.Close()
+    End Sub
+
+    Private Sub SoupButton_Click(sender As Object, e As EventArgs) Handles SoupButton.Click
+        'check to see if the previous value of the text box was a soup1. If yes, display soup 2
+        If DisplaySpecialLabel.Text = soup1 Then
+            DisplaySpecialLabel.Text = soup2
+        Else
+            DisplaySpecialLabel.Text = soup1
+            CheckBox1.Visible = False
+            DressingOptions.Visible = False
+            SideOptions.Visible = False
+            'hide all option boxes
+        End If
+        'change to picture of soup
+        PictureBox1.Image = My.Resources.soups
+    End Sub
+
+    Private Sub SaladButton_Click(sender As Object, e As EventArgs) Handles SaladButton.Click
+        If DisplaySpecialLabel.Text = salad1 Then
+            DisplaySpecialLabel.Text = salad2
+            DressingOptions.Visible = False
+        Else
+            DisplaySpecialLabel.Text = salad1
+            CheckBox1.Visible = False
+            DressingOptions.Visible = True
+            SideOptions.Visible = False
+            'only show dressing options
+        End If
+        'change picture to salad
+        PictureBox1.Image = My.Resources.salad
+    End Sub
+
+    Private Sub FishButton_Click(sender As Object, e As EventArgs) Handles FishButton.Click
+        'choose picture based on fish selected
+        If DisplaySpecialLabel.Text = fish1 Then
+            DisplaySpecialLabel.Text = fish2
+            PictureBox1.Image = My.Resources.chicken_shrimp
+            CheckBox1.Visible = True
+            SideOptions.Visible = False
+            'only show remove peppers checkbox
+        Else
+            DisplaySpecialLabel.Text = fish1
+            PictureBox1.Image = My.Resources.salmon_p
+            CheckBox1.Visible = False
+            DressingOptions.Visible = False
+            SideOptions.Visible = True
+            'only show side options radio buttons
+        End If
+    End Sub
+End Class
